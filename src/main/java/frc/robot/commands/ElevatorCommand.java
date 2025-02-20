885ac8ebf2edaf83ce4488a4f6dfff3a2e0f1fcb
Alejandro Alba <25alejalb1@cms.edu.do>
2025-02-20 17:01:16 -0400

    Elevator LimitSwitches

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -3,11 +3,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.subsystems.ElevatorSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PS4Controller;
 
 public class ElevatorCommand extends Command {
     private final ElevatorSubsystem elevator;
     private final PS4Controller controller;
+    DigitalInput bottomlimitSwitch = new DigitalInput(0);
+    DigitalInput toplimitSwitch = new DigitalInput(1);
 
     public ElevatorCommand(ElevatorSubsystem elevator, PS4Controller controller) {
         this.elevator = elevator;
@@ -17,12 +20,25 @@ public class ElevatorCommand extends Command {
 
     @Override
     public void execute() {
-        double upSpeed = controller.getR2Axis();  
-        double downSpeed = controller.getL2Axis(); 
+        double downSpeed;
+        double upSpeed;
+        
+        if (bottomlimitSwitch.get()){
+            downSpeed = 0;
+        } else {
+            downSpeed = controller.getL2Axis()/3; 
+        }
+
+        if (toplimitSwitch.get()){
+            upSpeed = 0;
+        } else {
+            upSpeed = controller.getR2Axis()/3;
+        }
+
         double speed = upSpeed - downSpeed; 
 
         elevator.setSpeed(speed);
-    }
+    } 
 
     @Override
     public void end(boolean interrupted) {
