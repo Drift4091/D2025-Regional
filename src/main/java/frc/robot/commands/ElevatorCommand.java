9b9d60d6766cedc30850f5dff4e0e7f777a534b2
Rafael Lindemberg <rafalindemberg12@gmail.com>
2025-03-03 15:43:46 -0400

    Update ElevatorCommand.java

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -1,22 +1,27 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-
 import frc.robot.subsystems.ElevatorSubsystem;
-import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.subsystems.led.LEDSubsystem; // Update to the correct package if needed
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorCommand extends Command {
     private final ElevatorSubsystem elevator;
+    private final LEDSubsystem leds;
     private final PS4Controller controller;
     private final double holdingForce = 0.01;
 
-
-    public ElevatorCommand(ElevatorSubsystem elevator, PS4Controller controller) {
+    public ElevatorCommand(ElevatorSubsystem elevator, LEDSubsystem leds, PS4Controller controller) {
         this.elevator = elevator;
+        this.leds = leds;
         this.controller = controller;
-        addRequirements(elevator);
+        addRequirements(elevator, leds);
+    }
+
+    @Override
+    public void initialize() {
+        leds.setColor(255, 0, 0); 
     }
 
     @Override
@@ -26,33 +31,28 @@ public class ElevatorCommand extends Command {
         SmartDashboard.putBoolean("Bottom Limit Switch", elevator.getBottomLimitSwitch());
         SmartDashboard.putBoolean("Top Limit Switch", elevator.getTopLimitSwitch());
 
-        if (elevator.getBottomLimitSwitch() || elevator.getEncoderPosition()<1){
+        if (elevator.getBottomLimitSwitch() || elevator.getEncoderPosition() < 1) {
             downSpeed = 0;
         } else {
-            downSpeed = (controller.getL2Axis()+1)/-4; 
+            downSpeed = (controller.getL2Axis() + 1) / -4;
         }
 
-        if (elevator.getTopLimitSwitch() || elevator.getEncoderPosition()>71){
+        if (elevator.getTopLimitSwitch() || elevator.getEncoderPosition() > 71) {
             upSpeed = 0;
         } else {
-            upSpeed = (controller.getR2Axis()+1)/4;
-            if (Math.abs(upSpeed) < holdingForce){  
+            upSpeed = (controller.getR2Axis() + 1) / 4;
+            if (Math.abs(upSpeed) < holdingForce) {
                 upSpeed = holdingForce;
             }
         }
 
-        double speed = upSpeed + downSpeed; 
-
+        double speed = upSpeed + downSpeed;
         elevator.setSpeed(speed);
-        
-    } 
+    }
 
-
-    // ale
-
-    
     @Override
     public void end(boolean interrupted) {
         elevator.stop();
+        leds.setDefaultColor(); // Reset LEDs back to neon green when elevator stops
     }
 }
