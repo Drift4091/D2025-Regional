7ea9c8003912d7f8af75fda816c6a967765c58ab
Rafael Lindemberg <rafalindemberg12@gmail.com>
2025-03-03 15:55:46 -0400

    Update ElevatorCommand.java

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -2,26 +2,22 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.led.LEDSubsystem; // Update to the correct package if needed
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorCommand extends Command {
     private final ElevatorSubsystem elevator;
-    private final LEDSubsystem leds;
     private final PS4Controller controller;
     private final double holdingForce = 0.012;
 
-    public ElevatorCommand(ElevatorSubsystem elevator, LEDSubsystem leds, PS4Controller controller) {
+    public ElevatorCommand(ElevatorSubsystem elevator, PS4Controller controller) {
         this.elevator = elevator;
-        this.leds = leds;
         this.controller = controller;
-        addRequirements(elevator, leds);
+        addRequirements(elevator);
     }
 
     @Override
     public void initialize() {
-        leds.setColor(255, 0, 0); 
     }
 
     @Override
@@ -53,6 +49,6 @@ public class ElevatorCommand extends Command {
     @Override
     public void end(boolean interrupted) {
         elevator.stop();
-        leds.setDefaultColor(); // Reset LEDs back to neon green when elevator stops
+        
     }
 }
